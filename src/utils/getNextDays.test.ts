6a3c63c9560e3567823d8a591f480988bd6eccea
Luis Carlos Ortellado <luiscarlosortecabral@gmail.com>
2023-08-20 21:34:58 -0400

    Add test getNextDays

---
+++ src/utils/getNextDays.test.ts
@@ -0,0 +1,8 @@
+import { getNextDays } from "./getNextDays"
+
+describe("getNextDays", ()=> {
+    it("should be return the next fixe days", ()=> {
+        const days = getNextDays()
+        expect(days.length).toBe(5)
+    })
+})
